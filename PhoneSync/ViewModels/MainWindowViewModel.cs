448bbe0a3eb7e3212ed9f5a9c88165b92ffed4ed
p.houot <>
2025-11-08 23:55:21 +0100

    init

---
+++ PhoneSync/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PhoneSync.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ PhoneSync/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PhoneSync.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
